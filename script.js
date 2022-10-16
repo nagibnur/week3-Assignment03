0fcb4397134023df04875d491036ee39affd363f
nagibnur <nagibnur@hotmail.com>
2022-10-16 19:46:08 +0100

    added upper characters letters, and test log

--- script.js
+++ script.js
@@ -2,6 +2,16 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+
+const charsUpperCase = [
+  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 
+  'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 
+  'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+];
+  
+console.log( charsUpperCase);
+
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
